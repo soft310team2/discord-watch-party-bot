35cbdc09c17931d40dbe22df1e17e29698c4b73d
klow358 <klow358@aucklanduni.ac.nz>
2023-09-21 00:26:14 +1200

    Added missing methods

--- bot.py
+++ bot.py
@@ -148,6 +148,163 @@ async def watchlist_view(interaction: nextcord.Interaction, watchlist_name):
     await interaction.response.send_message(response)
 
 # ---------------------------------------------------------------------------
+# Media Commands - Add tags to a specified media in a watchlist
+# ---------------------------------------------------------------------------
+@bot.slash_command(guild_ids=[GUILD_ID], name="add_tags", description="Add tags to a media in watchlist", default_member_permissions=EDIT_PERMISSION)
+async def add_tags(interaction: nextcord.Interaction, watchlist_name, media_name, tags):
+    """
+    Allows users to add tags to media items in a watchlist
+    Args:
+        interaction (nextcord.Interaction): The interaction object representing the command invocation.
+        watchlist_name str: The name of a watchlist in which the media item the user wants to add tags to is in
+        media_name str: The name of the media item  (Movie/Show/Animation) the user wants to add tags to
+        tags str: The tags the user wants to add to the media item in some watchlist
+
+    Returns:
+    None
+
+    """
+
+    # These are the tags from which the user can choose from
+    VALID_TAGS = ["movie", "show", "animation", "documentary", "horror", "thriller", "sci-fi", "fantasy", "mystery", "comedy", "action", "adventure"]
+
+    # Read in JSON data
+    watchlist_data = utils.read_watchlist_file(WATCHLISTFILENAME)
+    watchlist = utils.get_watchlist(watchlist_data, watchlist_name)
+
+    if watchlist:
+        if media_name in watchlist["media"]:
+            # Obtain the media item and parse the tags the user has specified
+            media = watchlist["media"].get(media_name)
+            new_tags = [tag.strip().lower() for tag in re.split(r'[ ,]+', tags)]
+            # Check which tags are valid/invalid from the ones provided by the user
+            valid_tags = [tag for tag in new_tags if tag in VALID_TAGS]
+            invalid_tags = [tag for tag in new_tags if tag not in VALID_TAGS]
+
+            # If there are valid tags from the parsed tags, add them to the current tags and ensure there are no duplicate tags
+            if valid_tags:
+                media["tags"].extend(valid_tags)
+                media["tags"] = list(set(media["tags"]))
+
+            # If there are any invalid tags, then inform the user and list the valid tags they can instead choose from
+            if invalid_tags:
+                response = f"Failed to add the following tags to *{media_name}* : **{', '.join(invalid_tags)}**. \n\nPlease select tags from the following: \n**{', '.join(VALID_TAGS)}**"
+            else:
+                response = f"Succesfully added all tags to *{media_name}*"
+
+        else:
+            response = f"*{media_name}* not found in **{watchlist_name}**!"
+    else:
+        response = f"The **{watchlist_name}** watchlist does not exist!"
+
+    utils.write_watchlist_file(WATCHLISTFILENAME, watchlist_data)
+    await interaction.response.send_message(response)
+
+
+@bot.slash_command(guild_ids=[GUILD_ID], name="delete_tags", description="Remove tags from a media in watchlist", default_member_permissions=EDIT_PERMISSION)
+async def delete_tags(interaction: nextcord.Interaction, watchlist_name, media_name, tags):
+    """
+    Allows users to delete tags from certain media items in some watchlist
+    Args:
+        interaction (nextcord.Interaction): The interaction object representing the command invocation.
+        watchlist_name: The name of the watchlist from which the user would like to delete tag(s)
+        media_name: The name of the media item in some watchlist from which the user would like to delete tag(s) from
+        tags: The existing tags in some media item in a watchlist
+
+    Returns:
+    None
+
+    """
+    # Read in the JSON data
+    watchlist_data = utils.read_watchlist_file(WATCHLISTFILENAME)
+    watchlist = utils.get_watchlist(watchlist_data, watchlist_name)
+
+    if watchlist:
+        # Ensure the media name is valid
+        if media_name in watchlist["media"]:
+            media = watchlist["media"].get(media_name)
+            # Note that this uses regex to split the input tags based on ',' or simply a blank space to allow flexability in input
+            deletion_tags = [tag.strip().lower() for tag in re.split(r'[ ,]+', tags)]
+
+            # The new tags for the specified media item after removing the ones the user wants to delete
+            new_tags = [tag for tag in media["tags"] if tag not in deletion_tags]
+            # We should also find the invalid tags from the input to inform the user
+            invalid_tags = [tag for tag in deletion_tags if tag not in media["tags"]]
+            media["tags"] = list(set(new_tags))
+
+            if invalid_tags:
+                response = f"The following tags are either invalid tags, or already do not exist in *{media_name}*, hence could not be removed: **{', '.join(invalid_tags)}**. *{media_name}* currently has the followng tags: \n"
+                response += "\n".join([f"- {name}" for name in media["tags"]])
+
+            else:
+                # Inform the user on the status of the tags for the specified media item
+                if len(media["tags"]) > 0:
+                    response = f"Tags successfully removed from *{media_name}*, *{media_name}* currently has the following tags:\n"
+                    response += "\n".join([f"- {name}" for name in media["tags"]])
+                else:
+                    response = f"Tags successfully removed from *{media_name}*, *{media_name}* has no more tags, you can add some with **/add_tags**"
+        else:
+            response = f"*{media_name}* could not be found in the **{watchlist_name}** watchlist"
+
+    else:
+        response = f"The **{watchlist_name}** watchlist does not exist!"
+
+    utils.write_watchlist_file(WATCHLISTFILENAME, watchlist_data)
+    await interaction.response.send_message(response)
+
+
+# ---------------------------------------------------------------------------
+# Media Commands - Update watch status of specified media to watchlist
+# ---------------------------------------------------------------------------
+
+@bot.slash_command(guild_ids=[GUILD_ID], name="status",
+                   description="update watch status of a movie or show in a watchlist")
+async def watchlist_update_status(interaction: nextcord.Interaction, watch_status, media_name, watchlist_name):
+    """
+    Updates the watch status of specified a movie or show in a watchlist.
+
+    Parameters:
+    interaction (nextcord.Interaction): The interaction object representing the command invocation.
+    watch_status (str): The watch status - unwatched, in progress or watched
+    media_name (str): The name of the movie or show to update watch status of.
+    watchlist_name (str): The name of the watchlist to which the movie or show to update watch status of
+
+    Returns:
+    None
+    """
+    # TODO: Provide user with 3 options menu instead
+    watch_status = watch_status.strip().lower()
+    valid_statuses = ["unwatched", "in progress", "watched"]
+
+    # Checks if valid status input
+    if watch_status in valid_statuses:
+
+        # Read the JSON data
+        watchlist_data = utils.read_watchlist_file(WATCHLISTFILENAME)
+
+        # Check if the watchlist exists
+        watchlist = utils.get_watchlist(watchlist_data, watchlist_name)
+        if (watchlist != None):
+            # Check if media already exists in watchlist
+            if media_name in watchlist["media"]:
+                media = watchlist["media"].get(media_name)
+                media["status"] = watch_status
+                response = f"Updated watch status of *{media_name}* to **{watch_status}** in the **{watchlist_name}** watchlist!"
+            else:
+                response = f"*{media_name}* is not in the **{watchlist_name}** watchlist! \nYou can add it with `/add {media_name}`"
+        else:
+            response = f"The **{watchlist_name}** watchlist does not exist! \nYou can create it with `/create {watchlist_name}`"
+
+        # Write the updated JSON data
+        utils.write_watchlist_file(WATCHLISTFILENAME, watchlist_data)
+
+        await interaction.response.send_message(response)
+
+    else:
+        await interaction.response.send_message(
+            "Invalid watch status. Please choose from: unwatched, in progress, watched")
+
+# ---------------------------------------------------------------------------
 # Participant Commands - Join, leave, view or notifty watchlist participants
 # ---------------------------------------------------------------------------
 
